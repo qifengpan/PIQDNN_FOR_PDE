d45d4814d2caaee3dcbbf585170117e991a51842
qifengpan <qifeng.pan@hlrs.de>
2023-04-20 21:34:57 +0200

    add model construction component

---
+++ src/utils/model.py
@@ -0,0 +1,70 @@
+import numpy as np
+import torch.nn.parameter import Parameter
+from IPython.display import clear_output
+from qiskit.circuit import ParameterVector
+import matplotlib.pyplot as plt
+
+def conv_circuit(params):
+    target = QuantumCircuit(2)
+    target.rz(-np.pi / 2, 1)
+    target.cx(1, 0)
+    target.rz(params[0], 0)
+    target.ry(params[1], 1)
+    target.cx(0, 1)
+    target.ry(params[2], 1)
+    target.cx(1, 0)
+    target.rz(np.pi / 2, 0)
+    return target
+def conv_layer(num_qubits, param_prefix):
+    qc = QuantumCircuit(num_qubits, name="Convolutional Layer")
+    qubits = list(range(num_qubits))
+    param_index = 0
+    params = ParameterVector(param_prefix, length=num_qubits * 3)
+    for q1, q2 in zip(qubits[0::2], qubits[1::2]):
+        qc = qc.compose(conv_circuit(params[param_index : (param_index + 3)]), [q1, q2])
+        qc.barrier()
+        param_index += 3
+    for q1, q2 in zip(qubits[1::2], qubits[2::2] + [0]):
+        qc = qc.compose(conv_circuit(params[param_index : (param_index + 3)]), [q1, q2])
+        qc.barrier()
+        param_index += 3
+
+    qc_inst = qc.to_instruction()
+
+    qc = QuantumCircuit(num_qubits)
+    qc.append(qc_inst, qubits)
+    return qc
+def pool_circuit(params):
+    target = QuantumCircuit(2)
+    target.rz(-np.pi / 2, 1)
+    target.cx(1, 0)
+    target.rz(params[0], 0)
+    target.ry(params[1], 1)
+    target.cx(0, 1)
+    target.ry(params[2], 1)
+
+    return target
+def pool_layer(sources, sinks, param_prefix):
+    num_qubits = len(sources) + len(sinks)
+    qc = QuantumCircuit(num_qubits, name="Pooling Layer")
+    param_index = 0
+    params = ParameterVector(param_prefix, length=num_qubits // 2 * 3)
+    for source, sink in zip(sources, sinks):
+        qc = qc.compose(pool_circuit(params[param_index : (param_index + 3)]), [source, sink])
+        qc.barrier()
+        param_index += 3
+
+    qc_inst = qc.to_instruction()
+
+    qc = QuantumCircuit(num_qubits)
+    qc.append(qc_inst, range(num_qubits))
+    return qc
+def callback_graph(weights, obj_func_eval):
+    clear_output(wait=True)
+    objective_func_vals.append(obj_func_eval)
+    plt.title("Objective function value against iteration")
+    plt.xlabel("Iteration")
+    plt.ylabel("Objective function value")
+    plt.plot(range(len(objective_func_vals)), objective_func_vals)
+    plt.show()
+
